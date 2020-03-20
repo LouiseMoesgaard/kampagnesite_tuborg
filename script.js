c70159d5d3bd254364ec5b1c98d6600db5f7ff9e
Louise Moesgaard Nielsen <louise@moesgaard.dk>
2020-03-20 22:52:14 +0100

    smårettelser

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let nav = document.querySelector("nav ul");
+let modal = document.querySelector(".egg_modal");
 
-document.addEventListener("DOMContentLoaded", function(){
-
-    if (window.screen.availWidth >= 600) {
+document.addEventListener("DOMContentLoaded", function () {
+    if (window.innerWidth >= 600) {
         nav.classList.remove("hide")
 
     } else {
@@ -10,18 +10,29 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 
     document.querySelector(".burger").addEventListener("click", function () {
-    nav.classList.remove("hide")
-})
+        nav.classList.remove("hide")
+    })
 
     document.querySelector(".close_burger").addEventListener("click", function () {
         nav.classList.add("hide")
     })
 
+    document.querySelector(".egg_btn").addEventListener("click", function () {
+        modal.style.height = (document.height || document.body.offsetHeight) + "px";
+        modal.firstElementChild.style.top = (window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop) + "px";
+
+        modal.classList.remove("hide");
+
+    })
+
+    document.querySelector(".close_btn").addEventListener("click", function () {
+        modal.classList.add("hide");
+    })
 });
 
 
-window.addEventListener("resize",function(){
-    if (window.screen.availWidth >= 600) {
+window.addEventListener("resize", function () {
+    if (window.innerWidth >= 600) {
         nav.classList.remove("hide")
 
     } else {
@@ -29,4 +40,3 @@ window.addEventListener("resize",function(){
 
     }
 });
-
